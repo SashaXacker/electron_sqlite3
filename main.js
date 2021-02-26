be2396565b39b82d30ce8a56e43c0ea049090f66
Linux <sashasuvorov12@yandex.ru>
2021-02-26 19:13:22 +0300

    v1.0

--- main.js
+++ main.js
@@ -8,15 +8,16 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true
+    },
   })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
@@ -41,3 +42,4 @@ app.on('window-all-closed', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
--- renderer.js
+++ renderer.js
@@ -1,6 +1,15 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('server.db', (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+    let sql = `SELECT inventNum, typetext, cabinet.number as cab, statustext, dataEdit FROM list
+                JOIN typelist ON list.type = typelist.id
+                JOIN statuslist ON list.status = statuslist.id
+                JOIN cabinet ON list.cab = cabinet.id`
+    db.each(sql, function(err, row) {
+        console.log(row.inventNum +' '+ row.typetext +' '+ row.cab +' '+ row.statustext +' '+ row.dataEdit);
+    });
+    db.close();
+});  
